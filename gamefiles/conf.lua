6c0b5c34ad8fdb94afac52f91fae2c14b67537f4
stevencalawaytisnm <steven.calaway@tisnm.org>
2025-09-04 11:24:43 -0600

    file movement

---
+++ gamefiles/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.console = true
+    t.window.width = 1920
+    t.window.height = 1200
+end
---
+++ gamefiles/main.lua
@@ -0,0 +1,118 @@
+
+function love.load()
+    snakeSegments = {
+        {x=10, y=7},
+        {x=9, y=7},
+        {x=8, y=7}
+    }
+   
+    time = 0
+    countdownTime = 100
+    timer = 0
+
+    directionQueue = {'right'}
+
+    cellSize = 40
+    gridXcount = math.floor(1920 / cellSize)
+    gridYcount = math.floor(1200 / cellSize)
+end
+
+function love.update(dt)
+    time = time + dt
+    countdownTime = countdownTime - dt
+
+    timer = timer + dt
+    if timer >= 0.15 then
+        timer = 0
+        time = time + dt
+        
+        if #directionQueue > 1 then
+            table.remove(directionQueue, 1)
+        end
+
+        local nextXPosition = snakeSegments[1].x
+        local nextYPosition = snakeSegments[1].y
+
+        if directionQueue[1] == 'right' then
+            nextXPosition = nextXPosition + 1
+            if nextXPosition > gridXcount then
+                nextXPosition = 1
+            end
+
+        elseif directionQueue[1] == 'left' then
+            nextXPosition = nextXPosition - 1
+            if nextXPosition < 1 then
+                nextXPosition = gridXcount
+            end
+
+        elseif directionQueue[1] == 'down' then
+            nextYPosition = nextYPosition + 1
+            if nextYPosition > gridYcount then
+                nextYPosition = 1
+            end
+
+        elseif directionQueue[1] == 'up' then
+            nextYPosition = nextYPosition - 1
+            if nextYPosition < 1 then
+                nextYPosition = gridYcount
+            end
+        end
+
+        table.insert(snakeSegments, 1, {
+            x = nextXPosition, y = nextYPosition    
+        })
+        table.remove(snakeSegments)
+    end
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+
+    if key == "right" 
+    and directionQueue[#directionQueue] ~= 'right'
+    and directionQueue[#directionQueue] ~= 'left' then 
+        table.insert(directionQueue, 'right')
+
+    elseif key == 'left' 
+    and directionQueue[#directionQueue] ~= 'left'
+    and directionQueue[#directionQueue] ~= 'right' then
+        table.insert(directionQueue, 'left')
+
+    elseif key == 'up'
+    and directionQueue[#directionQueue] ~= 'up' 
+    and directionQueue[#directionQueue] ~= 'down' then
+        table.insert(directionQueue, 'up')
+
+    elseif key == 'down' 
+    and directionQueue[#directionQueue] ~= 'down'
+    and directionQueue[#directionQueue] ~= 'up' then
+        table.insert(directionQueue, 'down')
+    end
+end
+
+function love.draw()
+    local cellSize = 40
+
+    love.graphics.setColor(1.0, 0.8, 0.9)
+    love.graphics.rectangle(
+        "fill",
+        0,
+        0,
+        gridXcount * cellSize,
+        gridYcount * cellSize
+    )
+
+    for segmentIndex, segment in ipairs(snakeSegments) do
+        love.graphics.setColor(148/255, 232/255, 240/255)
+        love.graphics.rectangle(
+            'fill',
+        (segment.x - 1) * cellSize,
+        (segment.y - 1) * cellSize,
+        cellSize - 1,
+        cellSize - 1
+        )
+    love.graphics.print("Time Left: " .. math.ceil(countdownTime), 10, 10)
+    end
+end
